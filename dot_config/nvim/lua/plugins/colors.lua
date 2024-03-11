4c7fd0452490d38edae9190c3beee8b2a6975faa
Evan Katsuranis <egkatsuranis@ucdavis.edu>
2024-03-11 15:00:22 -0700

    Switched to Dracula theme

--- dot_config/nvim/lua/plugins/colors.lua
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -1,3 +1,12 @@
 return {
   { "rebelot/kanagawa.nvim" },
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
 }
--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -11,7 +11,7 @@ config.font_size = 20
 config.max_fps = 150
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Dracula (base16)"
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
