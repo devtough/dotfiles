81c1f7ed0edfff05e6e5988260810d439019341c
Evan Katsuranis <egkatsuranis@ucdavis.edu>
2024-04-06 13:50:24 -0700

    Updated wezterm font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("JetBrains Mono", { Weight = "Bold" })
+config.font = wezterm.font("JetBrains Mono", { weight = "DemiBold" })
 config.font_size = 18
 config.max_fps = 150
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
@@ -17,7 +17,7 @@ config.window_decorations = "RESIZE"
 
 config.default_workspace = "~/Projects"
 
--- Dracula Color Pallette
+-- Dracula Color Palette
 -- from tmux for coloring tabs
 -- to match the tmux colors
 local white = "#f8f8f2"
@@ -34,14 +34,16 @@ local yellow = "#f1fa8c"
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
-config.show_new_tab_button_in_tab_bar = true
-config.tab_bar_at_bottom = false
-config.window_frame = {
-	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-	font_size = 18,
-	active_titlebar_bg = gray,
-	inactive_titlebar_bg = gray,
-}
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+-- could we replace other styling info above
+-- to a single map?
+--config.window_frame = {
+--	font = wezterm.font({ family = "JetBrains Mono" }),
+--	font_size = 18,
+--	active_titlebar_bg = gray,
+--	inactive_titlebar_bg = gray,
+--}
 config.colors = {
 	tab_bar = {
 		background = gray,
